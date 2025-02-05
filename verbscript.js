7dc82afb9626256af9dffa1d87fad05eef328072
Malhar Lathkar <mlathkar@gmail.com>
2025-02-05 09:50:56 +0530

    Update verbscript.js

--- verbscript.js
+++ verbscript.js
@@ -54,5 +54,6 @@ function retryImage(index) {
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('wordInput').setAttribute('autocomplete', 'off');
     displayRandomImage(); // Display a random image when the page loads
 });
